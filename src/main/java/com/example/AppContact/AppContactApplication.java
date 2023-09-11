3f3f311709641d8f54f75e88ae2e907bab6dcb98
ABDELKARIM2021 <elouaddany.k99@gmail.com>
2023-09-11 21:04:50 +0100

    Initial commit

---
+++ src/main/java/com/example/AppContact/AppContactApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppContact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppContactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AppContact/AppContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppContact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
