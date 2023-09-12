73db38662b7cbe52d32cc833ea68cfbc00f1de28
ABDELKARIM2021 <elouaddany.k99@gmail.com>
2023-09-12 13:24:22 +0100

    Create ContactController.java

---
+++ src/main/java/com/example/AppContact/contrllers/ContactController.java
@@ -0,0 +1,50 @@
+package com.example.AppContact.contrllers;
+²
+import com.example.AppContact.Service.ContactService;
+import com.example.AppContact.module.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contact")
+
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+
+    @GetMapping("/all")
+    public List<Contact> getAll(){
+        return contactService.getAll();
+    }
+
+    @PostMapping("/add")
+    public Contact save(@RequestBody Contact contact){
+       return contactService.save(contact);
+    }
+
+    @PutMapping("/update")
+    public  Contact update(@RequestBody Contact contact){
+        return contactService.update(contact);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Integer id){
+        contactService.delete(id);
+    }
+
+
+    @GetMapping("/{id}")
+    public Contact  findById(@PathVariable Integer id){
+        return contactService.findById(id);
+
+    }
+
+    @GetMapping("/nom/{nom}")
+    public  Contact findByNom(@PathVariable String nom){
+        return contactService.findByNom(nom);
+    }
+}
