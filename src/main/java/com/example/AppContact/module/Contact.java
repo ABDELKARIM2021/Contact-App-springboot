5836a0883d669eef834338ea1ce83f827e5eaee5
ABDELKARIM2021 <elouaddany.k99@gmail.com>
2023-09-12 13:23:49 +0100

    Create Contact.java

---
+++ src/main/java/com/example/AppContact/module/Contact.java
@@ -0,0 +1,25 @@
+package com.example.AppContact.module;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String nom;
+
+    private String prenom;
+}
